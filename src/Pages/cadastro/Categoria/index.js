16b4dcb73a6b1f46924fe3a236a73177f52bb2af
Matheus Rodrigues <36890610+Th3Matts@users.noreply.github.com>
2020-07-30 19:37:15 -0300

    Finalizado dia 2!

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria(){
+function CadastroCategoria() {
   return (
-   <PageDefault>
-    <h1> Cadastro de Categoria </h1>
-     
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-     
-     <Link to="/">
-        Ir para Home
-     </Link>
-   </PageDefault>
-    );
-  }
+      <form>
 
-  export default CadastroCategoria;
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
